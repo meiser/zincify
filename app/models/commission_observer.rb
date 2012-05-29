1cc6cd0bf0798c14d45ecb336694a6a5e924a62f
Stephan Keller <=>
2012-05-29 16:11:05 +0200

    test model observer commission

--- app/models/commission_observer.rb
+++ app/models/commission_observer.rb
@@ -1,13 +1,34 @@
 class CommissionObserver < ActiveRecord::Observer
 
+include Rails.application.routes.url_helpers
+
 #observe :commit
  def after_save(record)
   begin
     message = {:channel => "/messages/new", :data => SecureRandom.hex(16), :ext => {:auth_token => FAYE_TOKEN}}
     uri = URI.parse(FAYE_URL)
+    uri.path.gsub!(".js","")
+
     Net::HTTP.post_form(uri, :message => message.to_json)
   rescue
-    p "TODO: CREATE ASYNC, DELAYED JOB"
+    p "TODO: CREATE ASYNC, DELAYED JOB for simple message"
+  end
+ end
+
+ def after_update(record)
+  begin
+    message = {
+      :channel => "/#{record.id}",
+      :data => SecureRandom.hex(16),
+      :ext => {:auth_token => FAYE_TOKEN}
+    }
+
+    p message
+    uri = URI.parse(FAYE_URL)
+    uri.path.gsub!(".js","")
+    Net::HTTP.post_form(uri, :message => message.to_json)
+  rescue
+    p "TODO: CREATE ASYNC, DELAYED JOB for Status of commission"
   end
  end
 
