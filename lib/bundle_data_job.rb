1b600b17cb322804fbab6b20bfebb5d2bfb03897
Stephan Keller <=>
2012-06-15 15:35:17 +0200

    updated bundle data job

--- lib/bundle_data_job.rb
+++ lib/bundle_data_job.rb
@@ -2,9 +2,13 @@ class BundleDataJob < Struct.new(:delivery_number)
 
   def perform
 
-    Printer.create(:ident =>"0815",:description => "skeller")
-
-
+   db = Informix.connect(ENV["INFORMIXSERVER"], ENV["INFORMIXUSER"], ENV["INFORMIXPWD"])
+   db.foreach_hash("select FIRST 20000 t_nama, t_seak from ttccom100110") do |r|
+    Customer.create(
+     :name => r["t_nama"].force_encoding("UTF-8").strip,
+     :address => r["t_seak"].force_encoding("UTF-8").strip
+    )
+   end
 
   end
 
