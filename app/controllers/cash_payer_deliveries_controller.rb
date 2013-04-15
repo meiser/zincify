57c9f5a8aff07653542454e373b03ef1f1a21e62
Stephan <s.keller@meiser.de>
2013-04-15 13:08:57 +0200

    barzahler in etikett

--- app/controllers/cash_payer_deliveries_controller.rb
+++ app/controllers/cash_payer_deliveries_controller.rb
@@ -8,7 +8,7 @@ class CashPayerDeliveriesController < ApplicationController
   	t.printer = "0001"
   	t.label = "commission.btw"
 	if @cash_payer_delivery.custom_name.present?
-		t.data = "#{@cash_payer_delivery.commission}|#{@cash_payer_delivery.custom_name}|#{@cash_payer_delivery.custom_name}|#{l(@cash_payer_delivery.indate)}|#{l(@cash_payer_delivery.outdate)}|#{@cash_payer_delivery.remarks}|#{@cash_payer_delivery.tag}"
+		t.data = "#{@cash_payer_delivery.commission}|BARZAHLUNG BEI ABHOLUNG: #{@cash_payer_delivery.custom_name}|BARZAHLUNG BEI ABHOLUNG: #{@cash_payer_delivery.custom_name}|#{l(@cash_payer_delivery.indate)}|#{l(@cash_payer_delivery.outdate)}|#{@cash_payer_delivery.remarks}|#{@cash_payer_delivery.tag}"
   	else
 		t.data = "#{@cash_payer_delivery.commission}|#{@cash_payer_delivery.cash_payer.name}|#{[@cash_payer_delivery.cash_payer.name, @cash_payer_delivery.cash_payer.address].join(": ")}|#{l(@cash_payer_delivery.indate)}|#{l(@cash_payer_delivery.outdate)}|#{@cash_payer_delivery.remarks}|#{@cash_payer_delivery.tag}"
   	end
