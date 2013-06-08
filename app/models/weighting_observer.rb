28bee0da324da60734be61c9ba81e41e363e3888
skeller1 <s.keller@meiser.de>
2013-06-08 06:40:00 +0200

    allow scanning of barcodes for cash_payer and customer

--- app/models/weighting_observer.rb
+++ app/models/weighting_observer.rb
@@ -10,6 +10,11 @@ class WeightingObserver < ActiveRecord::Observer
 	t.data= "#{d.tag}|#{d.customer.name}|#{record.sort_list.number}. #{record.sort_list.description}|#{I18n.l(record.created_at, :format => :coupon)}|#{record.weight_brutto}|#{record.weight_tara}|#{record.weight_netto}|#{record.pid}|#{record.shift}"
 	t.save
    end
+   if record.barcode.starts_with?("B")
+	d = CashPayerDelivery.where(:commission => record.barcode).first
+	t.data= "#{d.tag}|#{d.custom_name.present? ? d.custom_name : d.cash_payer.name}|#{record.sort_list.number}. #{record.sort_list.description}|#{I18n.l(record.created_at, :format => :coupon)}|#{record.weight_brutto}|#{record.weight_tara}|#{record.weight_netto}|#{record.pid}|#{record.shift}"
+	t.save
+   end
   else
    t.data= "#{record.ref}||#{record.sort_list.number}. #{record.sort_list.description}|#{I18n.l(record.created_at, :format => :coupon)}|#{record.weight_brutto}|#{record.weight_tara}|#{record.weight_netto}|#{record.pid}|#{record.shift}"
    t.save
