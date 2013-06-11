b1c2c4b615edc7a7ade214071ff90c17d05bbf3c
meiser <s.keller@meiser.de>
2013-06-11 08:33:28 +0300

    Update shift.rb
    fixing wrong calculation of current shift

--- lib/shift.rb
+++ lib/shift.rb
@@ -9,19 +9,19 @@ class Shift
 		when 6..13
 			@description = 1
 			@start_time = @date.change(:hour => 6)
-			@end_time = @date.change(:hour => 13)
+			@end_time = @date.change(:hour => 13, :min => 59, :sec => 59)
 		when 14..21
 			@description = 2
 			@start_time = @date.change(:hour => 14)
-			@end_time = @date.change(:hour => 21)
+			@end_time = @date.change(:hour => 21, :min => 59, :sec => 59)
 		when 22..23
 			@description = 3
 			@start_time = @date.change(:hour => 22)
-			@end_time = @date.change(:day =>@date.day.next, :hour => 5)
+			@end_time = @date.change(:day =>@date.day.next, :hour => 5, :min => 59, :sec => 59)
 		when 0..5
 			@description = 3
 			@start_time = @date.change(:day =>(@date-1.day).day, :hour => 22)
-			@end_time = @date.change(:hour => 5)
+			@end_time = @date.change(:hour => 5, :min => 59, :sec => 59)
   end
   new(@description,@date)
  end
