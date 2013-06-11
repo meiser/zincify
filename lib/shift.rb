ccedeece0bf525ac883be2dc7f0c9caec00cadd6
meiser <s.keller@meiser.de>
2013-06-11 07:42:16 +0200

    Update shift.rb

--- lib/shift.rb
+++ lib/shift.rb
@@ -4,7 +4,6 @@ class Shift
  
  def self.now
   date = DateTime.now
-  #@date=@date.change(:hour => 22, :sec => 1)
   description = case date.hour
   	when 6..13 then 1
 	when 14..21 then 2
@@ -27,13 +26,9 @@ class Shift
 	@start_time = @date.change(:hour => 14)
 	@end_time = @date.change(:hour => 21, :min => 59, :sec => 59)
   when 3
-    @start_time = @date.change(:hour => 22)
+    	@start_time = @date.change(:hour => 22)
 	@end_time = @date.change(:day =>(@date+1.day).day, :hour => 5, :min => 59, :sec => 59)
   end
  end
  
 end
-
-
-
-#Weighting.where("sort_list_id <>36").where(:created_at => s.start_time..s.end_time).count)
