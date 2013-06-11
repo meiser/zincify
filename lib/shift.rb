1296a2cc59c2c6200c8b69ae4531de313088c764
meiser <s.keller@meiser.de>
2013-06-11 07:38:57 +0200

    Update shift.rb

--- lib/shift.rb
+++ lib/shift.rb
@@ -3,33 +3,21 @@ class Shift
  attr_reader :date, :description, :start_time, :end_time 
  
  def self.now
-  @date = DateTime.now
+  date = DateTime.now
   #@date=@date.change(:hour => 22, :sec => 1)
-  case @date.hour
-		when 6..13
-			@description = 1
-			@start_time = @date.change(:hour => 6)
-			@end_time = @date.change(:hour => 13, :min => 59, :sec => 59)
-		when 14..21
-			@description = 2
-			@start_time = @date.change(:hour => 14)
-			@end_time = @date.change(:hour => 21, :min => 59, :sec => 59)
-		when 22..23
-			@description = 3
-			@start_time = @date.change(:hour => 22)
-			@end_time = @date.change(:day =>@date.day.next, :hour => 5, :min => 59, :sec => 59)
-		when 0..5
-			@description = 3
-			@start_time = @date.change(:day =>(@date-1.day).day, :hour => 22)
-			@end_time = @date.change(:hour => 5, :min => 59, :sec => 59)
+  description = case date.hour
+  	when 6..13 then 1
+	when 14..21 then 2
+	when 22..23 then 3
+	when 0..5 then 3
   end
-  new(@description,@date)
+  new(description,date)
  end
 
  def initialize(shift, date)
   @date = date
   shift = shift.to_i.round
-  @description = shift.round.between?(1,3) ? "#{shift}" : "1"
+  @description = shift.between?(1,3) ? "#{shift}" : "1"
  
   case shift
   when 1
