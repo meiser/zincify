8fbc489d3e7e86fc5b5d7ca5ebc453cd43eabb2a
skeller1 <s.keller@meiser.de>
2013-07-26 15:30:09 +0200

    writing commission to weighting table

--- app/observers/weighting_observer.rb
+++ app/observers/weighting_observer.rb
@@ -5,6 +5,8 @@ class WeightingObserver < ActiveRecord::Observer
   t.printer = "0002"
   t.label = "wiegecoupon.btw"
   
+  # find commission
+  commission= ''
   if weighting.barcode.present?
   
 	tag = MeiserBundleTag.where(:barcode => weighting.barcode).first
@@ -15,29 +17,34 @@ class WeightingObserver < ActiveRecord::Observer
 		c = Customer.where(:bpid => "280000001").first
 		t.data= "#{md.tag}|#{c.name}|#{weighting.sort_list.number}. #{weighting.sort_list.description}|#{I18n.l(weighting.created_at, :format => :coupon)}|#{weighting.weight_brutto}|#{weighting.weight_tara}|#{weighting.weight_netto}|#{weighting.pid}|B #{weighting.barcode[-3,3].strip}"
 		t.save
+		commission= md.tag
 	else
 		cd = CustomerDelivery.where(:commission => weighting.barcode).first
 		if cd.present?
 			t.data= "#{cd.tag}|#{cd.customer.name}|#{weighting.sort_list.number}. #{weighting.sort_list.description}|#{I18n.l(weighting.created_at, :format => :coupon)}|#{weighting.weight_brutto}|#{weighting.weight_tara}|#{weighting.weight_netto}|#{weighting.pid}|"
 			t.save
+			commission= cd.tag
 		else
 			cpd = CashPayerDelivery.where(:commission => weighting.barcode).first
 			if cpd.present?
 				t.data= "#{cpd.tag}|#{cpd.custom_name.present? ? cpd.custom_name : cpd.cash_payer.name}|#{weighting.sort_list.number}. #{weighting.sort_list.description}|#{I18n.l(weighting.created_at, :format => :coupon)}|#{weighting.weight_brutto}|#{weighting.weight_tara}|#{weighting.weight_netto}|#{weighting.pid}|"
 				t.save
+				commission= cpd.tag
 			end
 		end
 	end
+	
+	weighting.update_column(:ref, commission)
   else
    t.data= "#{weighting.ref}||#{weighting.sort_list.number}. #{weighting.sort_list.description}|#{I18n.l(weighting.created_at, :format => :coupon)}|#{weighting.weight_brutto}|#{weighting.weight_tara}|#{weighting.weight_netto}|#{weighting.pid}|"
    t.save
   end
-
  end
  
  def after_save(weighting)
   if weighting.barcode.present?
 	Delayed::Job.enqueue BaanCompletionJob.new(weighting.barcode, weighting.weight_netto, weighting.created_at, "test")
+	
   end
  end
 
