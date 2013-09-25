f6d9388654ba098e3ec2d76780be72dda7a8d526
skeller1 <s.keller@meiser.de>
2013-09-25 09:34:55 +0200

    fix bundle arrival problem

--- app/observers/deliver_reference_observer.rb
+++ app/observers/deliver_reference_observer.rb
@@ -8,7 +8,7 @@ class DeliverReferenceObserver < ActiveRecord::Observer
 	Delayed::Job.enqueue BundleDataJob.new(record) 
   
 	#Rückmeldung Ankunft in Verzinkerei Plauen
-	Delayed::Job.enqueue BundleArrivalJob.new(record.name,record.created_at)
+	Delayed::Job.enqueue BundleArrivalJob.new(record.name,DateTime.now)
   end
   
  end
