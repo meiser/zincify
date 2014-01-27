769801183aacc9a59e45156049326cb01f71cc45
Stephan <s.keller@meiser.de>
2014-01-27 17:20:13 +0100

    fix endless observer loop using update_column

--- lib/bundle_info_job.rb
+++ lib/bundle_info_job.rb
@@ -12,15 +12,13 @@ class BundleInfoJob < Struct.new(:bundle)
    
    Meiser.foreach_baan("SELECT t_wght, t_verzink, t_dicke FROM ttibde915120 where t_load = ? and t_bund = ?",[delivery, bund]) do |bund|
 	# get weight bundle
-	bundle.weight_raw = bund["t_wght"]
+	bundle.update_column(:weight_raw, bund["t_wght"])
 	
 	#Besondere Verzinkung
 	Meiser.foreach_baan("SELECT tttadv401000.t_ctnm from tttadv401000 where t_cpac = ? and t_cdom = ? and t_vers = ? and t_rele = ? and t_cust = ? and t_cnst = ?",["ti","bde.verzink","B50C", "c", "mg1", bund["t_verzink"]]) do |domain|
 				#deutsche Bezeichnung des Enum ermitteln
 				Meiser.foreach_baan("SELECT tttadv402000.t_edes from tttadv402000 where t_clan = ? and t_cpac = ? and t_cdom = ? and t_vers = ? and t_rele = ? and t_cust = ? and t_ctnm = ?",["3", "ti","bde.verzink","B50C", "c", "mg1", domain["t_ctnm"]]) do |domain_text|
-	
-					bundle.info = domain_text["t_edes"]
-				
+					bundle.update_column(:info, domain_text["t_edes"])
 				end
 	end
 	
@@ -29,17 +27,9 @@ class BundleInfoJob < Struct.new(:bundle)
 				
 				#deutsche Bezeichnung des Enum ermitteln
 				Meiser.foreach_baan("SELECT tttadv402000.t_edes from tttadv402000 where t_clan = ? and t_cpac = ? and t_cdom = ? and t_vers = ? and t_rele = ? and t_cust = ? and t_ctnm = ?",["3", "ti","bde.dicke","B50C", "c", "mg1", domain["t_ctnm"]]) do |domain_text|
-	
-					bundle.zinc = domain_text["t_edes"]
-				
+					bundle.update_column(:zinc, domain_text["t_edes"])
 				end
 	end
-		
-	
-	
-	bundle.save
-	
-	
 	
    end
    
