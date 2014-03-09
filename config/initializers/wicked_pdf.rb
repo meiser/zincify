cf18800cb84f1ba50e5a68a1ff417fc6b60a7d54
skeller1 <s.keller@meiser.de>
2014-03-09 20:29:00 +0100

    remove wicked pdf

--- config/initializers/wicked_pdf.rb
+++
@@ -1,7 +0,0 @@
-﻿WickedPdf.config = {
-  #:wkhtmltopdf => '/usr/local/bin/wkhtmltopdf',
-  #:layout => "pdf.html",
-  
-  :exe_path => 'C:\Program Files (x86)\wkhtmltopdf\bin\wkhtmltopdf.exe'
-  #:exe_path => Rails.root.join("lib").join("wkhtmltox-win32_0.12.0-03c001d.exe").to_s
-}
