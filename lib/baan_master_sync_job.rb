e26f0212212cda8669216effe3d66732686182d2
Stephan Keller <MiStK@gmx.de>
2015-09-02 10:43:48 +0200

    Update baan_master_sync_job.rb
    Aufbau Datenbankverbindung bei Jobstart

--- lib/baan_master_sync_job.rb
+++ lib/baan_master_sync_job.rb
@@ -4,7 +4,8 @@
 		CashPayer.synchronize_with_baan
 		Customer.synchronize_with_baan
 		
-		BAAN_ORACLE.exec("select t$item, t$dsca from ln61.ttcibd001280") do |row|
+		oracle = OCI8.new(ENV["ORACLE_USER"], ENV["ORACLE_PASSWORD"], ENV["ORACLE_URL"])
+		oracle.exec("select t$item, t$dsca from ln61.ttcibd001280") do |row|
 			#nur Artikel Verzinkung beginnen mit V im Normteil Artikelcode
 			if row[0][9] == 'V'
 				item = ItemBaseData.find_or_initialize_by_item(row[0])
