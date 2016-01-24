8c2071264878da80f1d6248105a06dc6248e5ef4
skeller1 <s.keller@meiser.de>
2016-01-24 16:10:56 +0100

    update rails production config to rails 4 requirements

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,7 +9,7 @@ Zincify::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = true
+  config.serve_static_files = true
 
   # Compress JavaScripts and CSS
   config.assets.compress = false
@@ -60,6 +60,8 @@ Zincify::Application.configure do
   config.i18n.enforce_available_locales = false
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  
+  config.eager_load = true
 
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
